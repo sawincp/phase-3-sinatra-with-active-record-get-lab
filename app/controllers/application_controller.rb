636880abe21dc0ab38ed6d38fe259c17dc6169b3
Corey Sawin <sawincp@gmail.com>
2023-05-22 18:27:37 -0400

    commit tree 99b17436dfce3c37a23b0d924ee5331e180dc31d

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,33 @@
 class ApplicationController < Sinatra::Base
-
+  
+   # Add this line to set the Content-Type header for all responses
+   set :default_content_type, 'application/json'
+  
   # add routes
+  get '/bakeries' do
+    # get all the bakeries from the database
+    bakeries = Bakery.all
+    # send them back as a JSON array
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    # returns single bakery as JSON with baked goods nested in array.
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    # see the BakedGood class for the  method definition of `.by_price`
+    baked_goods = BakedGood.by_price
+    baked_goods.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+
+    most_expensive = BakedGood.by_price.first
+    most_expensive.to_json
+
+  end
 
 end
